36c23ea98f0974765c047162399b48d08c28472b
Dante Gates <dgates@O2O-PA-DGATES.local>
2018-05-21 16:42:05 -0400

    updated docs

--- example.py
+++ example.py
@@ -1,3 +1,29 @@
+
+"""
+This code demonstrates how to expose a pickled sklearn model as a REST
+API via porter.
+
+The model predictions can be obtained by sending POST requests with a payload
+such as
+
+    [
+        {"id": 101, "feature1": "foo", "feature2": "bar", "feature3": 1.0}
+    ]
+
+to the endpoint
+
+    localhost:8000/supa-dupa-model/prediction/
+
+The corresponding output has the format
+
+    [
+        "model_id": 'supa-dupa-model-v0',
+        "predicitons": [
+            {"id": 101, "prediction": 1001.01}
+        ]
+    ]
+"""
+
 from porter.datascience import WrappedModel, WrappedFeatureEngineer
 from porter.services import ModelApp, ServiceConfig
 
@@ -10,8 +36,8 @@ model_app = ModelApp()
 
 
 input_schema = {
-    'column1': 'str',
-    'column2': 'str',
+    'feature1': 'str',
+    'feature2': 'str',
     'column3': float
 }
 feature_engineer = WrappedFeatureEngineer.from_file(path='/path/to/feature_engineer.pkl')
@@ -56,5 +82,5 @@ model_app.add_service(service_config)
 
 if __name__ == '__main__':
     # you can run this with `gunicorn app:model_app.app`
-    # localhost:8000/preflight-index-model/prediction <- POST requests
+    # localhost:8000/supa-dupa-model/prediction <- POST requests
     model_app.app.run(port=8000)
