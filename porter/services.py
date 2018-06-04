1e575cfda4b80e8395f38f39661526a07d4e6ddc
Dante Gates <dgates@O2O-PA-DGATES.fios-router.home>
2018-06-04 10:15:32 -0400

    using class based routes for predictions rather than partials

--- porter/services.py
+++ porter/services.py
@@ -1,50 +1,61 @@
-from functools import partial
-
 import flask
 import pandas as pd
-from werkzeug.exceptions import BadRequest
 
-from porter.responses import make_prediction_response, make_error_response
+import porter.responses as porter_responses
 from porter import utils
 
 
 _ID_KEY = 'id'
 
 
-def check_request(X, feature_names, allow_nulls=False):
-    required_keys = [_ID_KEY]
-    required_keys.extend(feature_names)
-    # checks that all columns are present and no nulls sent
-    # (or missing values)
-    try:
-        # check for allow_nulls first to avoid computation if possible
-        if not allow_nulls and X[required_keys].isnull().any().any():
-            null_counts = X[required_keys].isnull().sum()
-            null_columns = null_counts[null_counts > 0].index.tolist()
-            raise ValueError(
-                'request payload had null values in the following fields: %s'
-                % null_columns)
-    except KeyError:
-        missing = [c for c in required_keys if not c in X.columns]
-        raise ValueError(
-            'request payload is missing the following fields: %s'
-            % missing)
+class ServePrediction(object):
+    def __init__(self, model, model_id, feature_engineer, input_schema,
+                 validate_input, allow_nulls):
+        self.model = model
+        self.model_id = model_id
+        self.feature_engineer = feature_engineer
+        self.input_schema = input_schema
+        self.validate_input = validate_input
+        self.allow_nulls = allow_nulls
+        self.transform_input = self.feature_engineer is not None
 
-def serve_prediction(model, model_id, feature_engineer, input_schema, validate_input,
-                     allow_nulls):
-    data = flask.request.get_json(force=True)
-    X = pd.DataFrame(data)
-    if validate_input:
-        check_request(X, input_schema.keys(), allow_nulls)
-    X_tf = X if feature_engineer is None else feature_engineer.transform(X)
-    model_prediction = model.predict(X_tf)
-    response = make_prediction_response(model_id, X[_ID_KEY], model_prediction)
-    return response
+    def __call__(self):
+        data = flask.request.get_json(force=True)
+        X = pd.DataFrame(data)
+        if self.validate_input:
+            self.check_request(X, self.input_schema.keys(), self.allow_nulls)
+        X_tf = self.feature_engineer.transform(X) if self.transform_input else X
+        model_prediction = self.model.predict(X_tf)
+        response = porter_responses.make_prediction_response(
+            self.model_id, X[_ID_KEY], model_prediction)
+        return response
+
+    @staticmethod
+    def check_request(X, feature_names, allow_nulls=False):
+        required_keys = [_ID_KEY]
+        required_keys.extend(feature_names)
+        # checks that all columns are present and no nulls sent
+        # (or missing values)
+        try:
+            # check for allow_nulls first to avoid computation if possible
+            if not allow_nulls and X[required_keys].isnull().any().any():
+                null_counts = X[required_keys].isnull().sum()
+                null_columns = null_counts[null_counts > 0].index.tolist()
+                raise ValueError(
+                    'request payload had null values in the following fields: %s'
+                    % null_columns)
+        except KeyError:
+            missing = [c for c in required_keys if not c in X.columns]
+            raise ValueError(
+                'request payload is missing the following fields: %s'
+                % missing)
+
 
 def serve_error_message(error):
     """Return a response with JSON payload describing the most recent exception."""
-    response = make_error_response(error)
+    response = porter_responses.make_error_response(error)
     return response
+
 
 def serve_alive():
     message = (
@@ -69,7 +80,7 @@ class ServiceConfig:
 
 
 class ModelApp:
-    _url_prediction_format = '/{endpoint}/prediction/'
+    _prediction_endpoint_template = '/{endpoint}/prediction/'
     _error_codes = (
         400,  # bad request
         404,  # not found
@@ -78,33 +89,34 @@ class ModelApp:
     )
 
     def __init__(self):
-        self.app = self._build_app()
+        self._build_app()
 
     def add_service(self, service_config):
-        model_url = self._make_model_url(service_config)
-        fn = self._make_prediction_fn(service_config)
-        self.app.route(model_url, methods=['POST'])(fn)  # calling flask route decorator directly
+        prediction_endpoint = self._prediction_endpoint_template.format(
+            endpoint=service_config.endpoint)
+        self._route_prediction(service_config, prediction_endpoint)
+
+    def _route_prediction(self, service_config, prediction_endpoint):
+        serve_prediction = ServePrediction(
+            model=service_config.model,
+            model_id=service_config.model_id,
+            feature_engineer=service_config.feature_engineer,
+            input_schema=service_config.input_schema,
+            validate_input=service_config.validate_input,
+            allow_nulls=service_config.allow_nulls)
+        # calling flask route decorator directly
+        self.app.route(prediction_endpoint, methods=['POST'])(serve_prediction)
 
     def _build_app(self):
-        app = flask.Flask(__name__)
+        self.app = flask.Flask(__name__)
         # register a custom JSON encoder that handles numpy data types.
-        app.json_encoder = utils.NumpyEncoder
+        self.app.json_encoder = utils.NumpyEncoder
         # register error handlers
         for error in self._error_codes:
-            app.register_error_handler(error, serve_error_message)
+            self.app.register_error_handler(error, serve_error_message)
         # create a route that can be used to check if the app is running
-        # usefule for kubernetes/helm integration
-        app.route('/alive/', methods=['GET'])(serve_alive)
-        return app
+        # useful for kubernetes/helm integration
+        self._route_alive()
 
-    def _make_model_url(self, service_config):
-        return self._url_prediction_format.format(endpoint=service_config.endpoint)
-
-    def _make_prediction_fn(self, service_config):
-        cf = service_config  # just an alias for convenience
-        fn = partial(serve_prediction, model=cf.model, model_id=cf.model_id,
-            feature_engineer=cf.feature_engineer, input_schema=cf.input_schema,
-            validate_input=cf.validate_input, allow_nulls=cf.allow_nulls)
-        # mimic function API - assumed in flask implementation
-        fn.__name__ = '{}_prediction'.format(cf.endpoint.replace('-', '_'))
-        return fn
+    def _route_alive(self):
+        self.app.route('/alive/', methods=['GET'])(serve_alive)
--- tests/test_services.py
+++ tests/test_services.py
@@ -1,67 +1,66 @@
-import mock
 import unittest
 
+import mock
 import numpy as np
 import pandas as pd
-from porter.services import (_ID_KEY, ModelApp, check_request,
-                          serve_error_message, serve_prediction)
+from porter.services import (_ID_KEY, ModelApp, ServePrediction,
+                             serve_error_message)
 
 
 class TestFuntions(unittest.TestCase):
+    def test_serve_error_message(self):
+        pass
+
+
+class TestServePrediction(unittest.TestCase):
     def test_check_request_pass(self):
         # no error should be raised
         X = pd.DataFrame(
             [[0, 1, 2, 3], [4, 5, 6, 7]],
             columns=[_ID_KEY, 'one', 'two', 'three'])
-        check_request(X, ['one', 'two', 'three'])
+        ServePrediction.check_request(X, ['one', 'two', 'three'])
 
     def test_check_request_fail_missing_id(self):
         X = pd.DataFrame(
             [[0, 1, 2, 3], [4, 5, 6, 7]],
             columns=['missing', 'one', 'two', 'three'])
         with self.assertRaises(ValueError):
-            check_request(X, ['one', 'two', 'three'])
+            ServePrediction.check_request(X, ['one', 'two', 'three'])
 
     def test_check_request_fail_missing_id_column(self):
         X = pd.DataFrame(
             [[0, 1, 2, 3], [4, 5, 6, 7]],
             columns=['missing', 'one', 'two', 'three'])
         with self.assertRaisesRegexp(ValueError, 'missing.*id'):
-            check_request(X, ['one', 'two', 'three'])
+            ServePrediction.check_request(X, ['one', 'two', 'three'])
 
     def test_check_request_fail_missing_input_columns(self):
         X = pd.DataFrame(
             [[0, 1, 2, 3], [4, 5, 6, 7]],
             columns=[_ID_KEY, 'missing', 'missing', 'three'])
         with self.assertRaisesRegexp(ValueError, 'missing.*one.*two'):
-            check_request(X, ['one', 'two', 'three'])
+            ServePrediction.check_request(X, ['one', 'two', 'three'])
 
     def test_check_request_fail_nulls(self):
         X = pd.DataFrame(
             [[0, 1, np.nan, 3], [4, 5, 6, np.nan]],
             columns=[_ID_KEY, 'one', 'two', 'three'])
         with self.assertRaisesRegexp(ValueError, 'null.*two.*three'):
-            check_request(X, ['one', 'two', 'three'])
+            ServePrediction.check_request(X, ['one', 'two', 'three'])
 
     def test_check_request_ignore_nulls_pass(self):
         X = pd.DataFrame(
             [[0, 1, np.nan, 3], [4, 5, 6, np.nan]],
             columns=[_ID_KEY, 'one', 'two', 'three'])
         # no error shoudl be raised
-        check_request(X, ['one', 'two', 'three'], True)
+        ServePrediction.check_request(X, ['one', 'two', 'three'], True)
 
     def test_check_request_ignore_nulls_no_check(self):
         # check that the computation counting nulls is never performed
         mock_X = mock.Mock()
         # no error shoudl be raised
-        check_request(mock_X, ['one', 'two', 'three'], True)
+        ServePrediction.check_request(mock_X, ['one', 'two', 'three'], True)
         mock_X.isnull.assert_not_called()
-
-    def test_serve_prediction(self):
-        pass
-
-    def test_serve_error_message(self):
-        pass
 
 
 class TestModelService(unittest.TestCase):
