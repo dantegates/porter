8d0cb872394b5f8509fcefc2148967b41f77cae7
Dante Gates <dgates@O2O-PA-DGATES.fios-router.home>
2018-06-07 10:08:33 -0400

    added large end to end test for user API

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,138 @@ import unittest
 
 import flask
 
-from porter.services import ModelApp
+from porter.datascience import BaseModel, BaseProcessor
+from porter.services import ModelApp, ServiceConfig, _ID_KEY
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.model_app = ModelApp()
+        self.app = self.model_app.app.test_client()
+
+    def test(self):
+        # define objects for model 1
+        class Preprocessor1(BaseProcessor):
+            def process(self, X):
+                X['feature2'] = X.feature2.astype(str)
+                return X
+        class Model1(BaseModel):
+            feature2_map = {str(x+1): x for x in range(5)}
+            def predict(self, X):
+                return X['feature1'] * X.feature2.map(self.feature2_map)
+        class Postprocessor1(BaseProcessor):
+            def process(self, X):
+                return X * -1
+        input_schema1 = ['feature1', 'feature2']
+        post_data1 = [
+            {_ID_KEY: 1, 'feature1': 2, 'feature2': 1},
+            {_ID_KEY: 2, 'feature1': 2, 'feature2': 2},
+            {_ID_KEY: 3, 'feature1': 2, 'feature2': 3},
+            {_ID_KEY: 4, 'feature1': 2, 'feature2': 4},
+            {_ID_KEY: 5, 'feature1': 2, 'feature2': 5},
+        ]
+
+        # define objects for model 2
+        class Preprocessor2(BaseProcessor):
+            def process(self, X):
+                X['feature3'] = range(len(X))
+                return X
+        class Model2(BaseModel):
+            def predict(self, X):
+                return X['feature1'] + X['feature3']
+        input_schema2 = ['feature1']
+        post_data2 = [
+            {_ID_KEY: 1, 'feature1': 10},
+            {_ID_KEY: 2, 'feature1': 10},
+            {_ID_KEY: 3, 'feature1':  1},
+            {_ID_KEY: 4, 'feature1':  3},
+            {_ID_KEY: 5, 'feature1':  3},
+        ]
+
+        # define objects for model 3
+        class Model3(BaseModel):
+            def predict(self, X):
+                return X['feature1'] * -1
+        input_schema3 = ['feature1']
+        post_data3 = [
+            {_ID_KEY: 1, 'feature1': 5},
+            {_ID_KEY: 2, 'feature1': 4},
+            {_ID_KEY: 3, 'feature1': 3},
+            {_ID_KEY: 4, 'feature1': 2},
+            {_ID_KEY: 5, 'feature1': 1},
+        ]
+
+        # define configs and add services to app
+        service_config1 = ServiceConfig(
+            model=Model1(),
+            endpoint='model-1',
+            model_id='model-1-id',
+            preprocessor=Preprocessor1(),
+            postprocessor=Postprocessor1(),
+            input_schema=input_schema1,
+            allow_nulls=False
+        )
+        service_config2 = ServiceConfig(
+            model=Model2(),
+            endpoint='model-2',
+            model_id='model-2-id',
+            preprocessor=Preprocessor2(),
+            postprocessor=None,
+            input_schema=input_schema2,
+            allow_nulls=False
+        )
+        service_config3 = ServiceConfig(
+            model=Model3(),
+            endpoint='model-3',
+            model_id='model-3-id',
+            preprocessor=None,
+            postprocessor=None,
+            input_schema=input_schema3,
+            allow_nulls=False
+        )
+        self.model_app.add_service(service_config1)
+        self.model_app.add_service(service_config2)
+        self.model_app.add_service(service_config3)
+
+        actual1 = self.app.post('/model-1/prediction/', data=json.dumps(post_data1))
+        actual1 = json.loads(actual1.data)
+        actual2 = self.app.post('/model-2/prediction/', data=json.dumps(post_data2))
+        actual2 = json.loads(actual2.data)
+        actual3 = self.app.post('/model-3/prediction/', data=json.dumps(post_data3))
+        actual3 = json.loads(actual3.data)
+        expected1 = {
+            'model_id': 'model-1-id',
+            'predictions': [
+                {_ID_KEY: 1, 'prediction': 0},
+                {_ID_KEY: 2, 'prediction': -2},
+                {_ID_KEY: 3, 'prediction': -4},
+                {_ID_KEY: 4, 'prediction': -6},
+                {_ID_KEY: 5, 'prediction': -8},
+            ]
+        }
+        expected2 = {
+            'model_id': 'model-2-id',
+            'predictions': [
+                {_ID_KEY: 1, 'prediction': 10},
+                {_ID_KEY: 2, 'prediction': 11},
+                {_ID_KEY: 3, 'prediction': 3},
+                {_ID_KEY: 4, 'prediction': 6},
+                {_ID_KEY: 5, 'prediction': 7},
+            ]
+        }
+        expected3 = {
+            'model_id': 'model-3-id',
+            'predictions': [
+                {_ID_KEY: 1, 'prediction': -5},
+                {_ID_KEY: 2, 'prediction': -4},
+                {_ID_KEY: 3, 'prediction': -3},
+                {_ID_KEY: 4, 'prediction': -2},
+                {_ID_KEY: 5, 'prediction': -1},
+            ]
+        }
+        self.assertEqual(actual1, expected1)
+        self.assertEqual(actual2, expected2)
+        self.assertEqual(actual3, expected3)        
 
 
 class TestAppErrorHandling(unittest.TestCase):
