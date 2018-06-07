21b6ab81e7de1dc8b39664cbf4cdc23f2ebe4d07
Dante Gates <dgates@O2O-PA-DGATES.fios-router.home>
2018-06-07 11:07:48 -0400

    added docstrings

--- porter/datascience.py
+++ porter/datascience.py
@@ -1,19 +1,26 @@
+"""
+"""
+
+
 import os
 
 
 # on the reasonableness of imports inside a function, see
 # https://stackoverflow.com/questions/3095071/in-python-what-happens-when-you-import-inside-of-a-function/3095167#3095167
 def load_pkl(path):
+    """Load and return a pickled object with `joblib`."""
     from sklearn.externals import joblib
     model = joblib.load(path)
     return model
 
 def load_h5(path):
+    """Load and return an object stored in h5 with `keras`."""
     import keras
     model = keras.models.load_model(path)
     return model
 
 def load_file(path):
+    """Load a file and return the result."""
     extension = os.path.splitext(path)[-1]
     if extension == '.pkl':
         obj = load_pkl(path)
@@ -25,18 +32,25 @@ def load_file(path):
 
 
 class BaseModel(object):
+    """Class defining the model interface required by
+        `porter.services.ModelApp.add_service`."""
     def predict(self, X):
         raise NotImplementedError(
             '%s must implement .predict()' % self.__class__.__name__)
 
 
 class BaseProcessor(object):
+    """Class defining the [pre|post]processor interface required by
+        `porter.services.ModelApp.add_service`."""
     def process(self, X):
         raise NotImplementedError(
             '%s must implement .process()' % self.__class__.__name__)
 
 
 class WrappedModel(BaseModel):
+    """A convenience class that exposes a model persisted to disk with the
+    `BaseModel` interface.
+    """
     def __init__(self, model):
         self.model = model
         super(WrappedModel, self).__init__()
@@ -51,6 +65,9 @@ class WrappedModel(BaseModel):
 
 
 class WrappedTransformer(BaseProcessor):
+    """A convenience class that exposes a transformer persisted to disk with
+    the `BaseProcessor` interface.
+    """
     def __init__(self, transformer):
         self.transformer = transformer
         super(WrappedTransformer, self).__init__()
