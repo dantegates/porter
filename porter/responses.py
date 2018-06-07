e6daf57ad3b22cda64fb36882c053896cede407b
Dante Gates <dgates@O2O-PA-DGATES.local>
2018-06-07 15:49:26 -0400

    removed response classes

--- porter/responses.py
+++ porter/responses.py
@@ -3,45 +3,32 @@ import traceback
 import flask
 
 
-class BasePayload(dict):
-    def __init__(self, *args, **kwargs):
-        payload = self._init_payload(*args, **kwargs)
-        super(BasePayload, self).__init__(payload)
-
-    def _init_payload(self):
-        raise NotImplementedError
-
-
-class PredictionPayload(BasePayload):
-    @staticmethod
-    def _init_payload(model_id, id_keys, predictions):
-        return {
-            'model_id': model_id,
-            'predictions': [{"id": id, "prediction": p}
-                            for id, p in zip(id_keys, predictions)]
-        }
-
-
-class ErrorPayload(BasePayload):
-    @staticmethod
-    def _init_payload(error):
-        return {
-            'error': type(error).__name__,
-            # getattr() is used to work around werkzeug's bad implementation
-            # of HTTPException (i.e. HTTPException inherits from Exception but
-            # exposes a different API, namely
-            # Exception.message -> HTTPException.description).
-            'message': getattr(error, 'description', error.message),
-            'traceback': traceback.format_exc()}
-
-
 def make_prediction_response(model_id, id_keys, predictions):
-    payload = PredictionPayload(model_id, id_keys, predictions)
+    payload = _make_prediction_payload(model_id, id_keys, predictions)
     return flask.jsonify(payload)
 
 
+def _make_prediction_payload(model_id, id_keys, predictions):
+    return {
+        'model_id': model_id,
+        'predictions': [{"id": id, "prediction": p}
+                        for id, p in zip(id_keys, predictions)]
+    }
+
+
 def make_error_response(error):
-    payload = ErrorPayload(error)
+    payload = _make_error_payload(error)
     response = flask.jsonify(payload)
     response.status_code = getattr(error, 'code', 500)
     return response
+
+
+def _make_error_payload(error):
+    return {
+        'error': type(error).__name__,
+        # getattr() is used to work around werkzeug's bad implementation
+        # of HTTPException (i.e. HTTPException inherits from Exception but
+        # exposes a different API, namely
+        # Exception.message -> HTTPException.description).
+        'message': getattr(error, 'description', error.message),
+        'traceback': traceback.format_exc()}
--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,24 +1,13 @@
 import json
 import unittest
 
-from porter.responses import PredictionPayload, ErrorPayload
+from porter.responses import (_make_error_payload, _make_prediction_payload,
+                              make_error_response, make_prediction_response)
 
 
-class TestPredictionPayload(unittest.TestCase):
-    def test_constructor(self):
-        actual = PredictionPayload(123, [1, 2, 3], [10.0, 11.0, 12.0])
-        expected = {
-            'model_id': 123,
-            'predictions': [
-                {1: 10.0},
-                {2: 11.0},
-                {3: 12.0}
-            ]
-        }
-        self.assertItemsEqual(actual, expected)
-
-    def test__init_payload(self):
-        actual = PredictionPayload(123, [1, 2, 3], [10.0, 11.0, 12.0])
+class TestFunctions(unittest.TestCase):
+    def test__make_prediction_payload(self):
+        actual = _make_prediction_payload(123, [1, 2, 3], [10.0, 11.0, 12.0])
         expected = {
             'model_id': 123,
             'predictions': [
@@ -29,75 +18,18 @@ class TestPredictionPayload(unittest.TestCase):
         }
         self.assertItemsEqual(actual, expected)
 
-    def test_json_compatability(self):
-        response = PredictionPayload(123, [1, 2, 3], [10.0, 11.0, 12.0])
-        actual_json = json.dumps(response)
-        # rather than comparing strings (which would include using OrderedDicts
-        # and writing out the JSON as a str) we simply decoded the encoded JSON.
-        #
-        # If PredictionPayload is incompatible with `json.dumps` either an error
-        # will be raised or the expected decoded object will not match what
-        # actually happened.
-        # In either event the test will fail.
-        actual_decoded_json = json.loads(actual_json)
-        expected_decoded_json = {
-            'model_id': 123,
-            'predictions': [
-                {"id": 1, "prediction": 10.0},
-                {"id": 2, "prediction": 11.0},
-                {"id": 3, "prediction": 12.0}
-            ]
-        }
-        self.assertItemsEqual(actual_decoded_json, expected_decoded_json)
-
-
-class TestErrorPayload(unittest.TestCase):
-    def test_constructor(self):
+    def test__make_error_payload(self):
         error = Exception('foo bar baz')
         try:
             raise error
-        except:
-            actual = ErrorPayload(error)
+        except Exception:
+            actual = _make_error_payload(error)
         expected = {
             'error': 'Exception',
             'message': 'foo bar baz',
             'traceback': 'raise error'
         }
-
-    def test__init_payload(self):
-        error = Exception('foo bar baz')
-        try:
-            raise error
-        except:
-            actual = ErrorPayload._init_payload(error)
-        expected = {
-            'error': 'Exception',
-            'message': 'foo bar baz',
-            'traceback': 'raise error'
-        }
-
-
-    def test_json_compatability(self):
-        error = Exception('foo bar baz')
-        try:
-            raise error
-        except:
-            response = ErrorPayload._init_payload(error)
-        actual_json = json.dumps(response)
-        # rather than comparing strings (which would include using OrderedDicts
-        # and writing out the JSON as a str) we simply decoded the encoded JSON.
-        #
-        # If PredictionPayload is incompatible with `json.dumps` either an error
-        # will be raised or the expected decoded object will not match what
-        # actually happened.
-        # In either event the test will fail.
-        actual_decoded_json = json.loads(actual_json)
-        expected_decoded_json = {
-            'error': 'Exception',
-            'message': 'foo bar baz',
-            'traceback': 'raise error'
-        }
-        self.assertItemsEqual(actual_decoded_json, expected_decoded_json)
+        self.assertItemsEqual(actual, expected)
 
 
 if __name__ == '__main__':
