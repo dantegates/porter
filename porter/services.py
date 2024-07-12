ed90791247fcf53069ccaaba58434e357cfdb8aa
dantegates <dantedgates@gmail.com>
2024-07-12 15:19:07 -0400

    Add hook to format response in `PredictionService` (#9)
    * add PredictionService._format_response
    
    * make _format_response signature accept X_input, X_preprocessed, preds
    
    * docstrings
    
    * make method private
    
    * bump version
    
    ---------
    
    Co-authored-by: Dante Gates <dantegates@dantes-mbp.local.meter>

--- porter/services.py
+++ porter/services.py
@@ -715,12 +715,7 @@ class PredictionService(BaseService):
             preds = self.postprocessor.process(X_input, X_preprocessed, preds)
 
         # finally format the predictions and return
-        if self.batch_prediction:
-            response = porter_responses.make_batch_prediction_response(X_input[_ID], preds)
-        else:
-            response = porter_responses.make_prediction_response(X_input[_ID].iloc[0], preds[0])
-
-        return response
+        return self._format_response(X_input, X_preprocessed, preds)
 
     def get_post_data(self):
         """Return data from the most recent POST request as a ``pandas.DataFrame``.
@@ -734,6 +729,28 @@ class PredictionService(BaseService):
         if not self.batch_prediction:
             data = [data]
         return pd.DataFrame(data)
+
+    def _format_response(self, X_input, X_preprocessed, preds):
+        """
+        Reshape predictions in "response format" accordingly for batch or instance
+        prediction.
+
+        Args:
+            X_input (``pandas.DataFrame``): The raw input from a POST request
+                converted to a ``pandas.DataFrame``.
+            X_preprocessed: The POST request data with preprocessing applied.
+            predictions: The output of an instance of :class:`BaseModel`.
+
+        Returns:
+            object: A "jsonified" object representing the response to return
+                to the user.
+        """
+        id_ = X_input[_ID]
+        if self.batch_prediction:
+            response = porter_responses.make_batch_prediction_response(id_, preds)
+        else:
+            response = porter_responses.make_prediction_response(id_.iloc[0], preds[0])
+        return response
 
     def _add_feature_schema(self, user_schema):
         assert isinstance(user_schema, schemas.Object), '``feature_schema`` must be an Object'
