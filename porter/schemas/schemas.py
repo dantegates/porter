4743d43fb2574a22226c1eae5d1a50b993883df9
Dante Gates <dantegates@dantes-mbp.local.meter>
2024-09-04 15:22:51 -0400

    reference schema keys from constants

--- porter/schemas/schemas.py
+++ porter/schemas/schemas.py
@@ -1,4 +1,5 @@
 from .. import config as cf
+from .. import constants as cn
 from . import openapi
 
 
@@ -16,9 +17,9 @@ model_meta = openapi.Object(properties={}, additional_params=dict(additionalProp
 
 model_context = openapi.Object(
     properties={
-        'model_name': openapi.String('The name of the model.'),
-        'api_version': openapi.String('The model API version.'),
-        'model_meta': model_meta
+        cn.MODEL_CONTEXT_KEYS.MODEL_NAME: openapi.String('The name of the model.'),
+        cn.MODEL_CONTEXT_KEYS.API_VERSION: openapi.String('The model API version.'),
+        cn.MODEL_CONTEXT_KEYS.MODEL_META: model_meta
     },
     reference_name='ModelContext'
 )
@@ -55,17 +56,17 @@ health_check = openapi.Object(
     'Description of the applications status. Useful for load balancing and debugging',
     properties={
         **_base_response,
-        'porter_version': openapi.String('The version of the porter on the deployed application.'),
-        'deployed_on': openapi.String('Start up time of the server. Format YYYY-MM-DDTHH:MM:SS.ffffff, e.g. 2020-04-01T19:00:31.518627'),
-        'app_meta': app_meta,
-        'services': openapi.Object(
+        cn.HEALTH_CHECK_KEYS.PORTER_VERSION: openapi.String('The version of the porter on the deployed application.'),
+        cn.HEALTH_CHECK_KEYS.DEPLOYED_ON: openapi.String('Start up time of the server. Format YYYY-MM-DDTHH:MM:SS.ffffff, e.g. 2020-04-01T19:00:31.518627'),
+        cn.HEALTH_CHECK_KEYS.APP_META: app_meta,
+        cn.HEALTH_CHECK_KEYS.SERVICES: openapi.Object(
             'All available services on the server',
             additional_properties_type=openapi.Object(
                 properties={
-                    'endpoint': openapi.String('Endpoint the service is exposed on.'),
-                    'status': openapi.String('Status of the model. If the app is ready the value will be "READY" .'
+                    cn.HEALTH_CHECK_SERVICES_KEYS.ENDPOINT: openapi.String('Endpoint the service is exposed on.'),
+                    cn.HEALTH_CHECK_SERVICES_KEYS.STATUS: openapi.String('Status of the model. If the app is ready the value will be "READY" .'
                                              'Otherwise the value will be a string indicating the status of the service.'),
-                    'model_context': model_context
+                    cn.HEALTH_CHECK_SERVICES_KEYS.MODEL_CONTEXT: model_context
                 }
             )
         )
@@ -85,7 +86,7 @@ error_body = openapi.Object(
 generic_error = openapi.Object(
     properties={
         **_base_response,
-        'error': error_body,
+        cn.GENERIC_ERROR_KEYS.ERROR: error_body,
     },
     reference_name='GenericError'
 )
@@ -94,8 +95,8 @@ generic_error = openapi.Object(
 model_context_error = openapi.Object(
     properties={
         **_base_response,
-        'error': error_body,
-        'model_context': model_context
+        cn.GENERIC_ERROR_KEYS.ERROR: error_body,
+        cn.MODEL_CONTEXT_ERROR_KEYS.MODEL_CONTEXT: model_context
     },
     reference_name='ModelContextError'
 )
